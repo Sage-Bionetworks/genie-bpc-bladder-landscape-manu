17118b75147bb12c0e51811e5f863308d25198fa
alexpaynter <alexcpaynter@gmail.com>
2023-06-21 01:44:23 +0000

    Starting exploratory work on timing

---
+++ R/get_first_cpt.R
@@ -0,0 +1,12 @@
+get_first_cpt <- function(ca_ind_dat, cpt_dat) {
+  rtn <- ca_ind_dat %>% 
+    select(record_id, ca_seq) %>%
+    left_join(., cpt_dat, by = c("record_id", "ca_seq")) %>%
+    arrange(cpt_number) %>%
+    group_by(record_id) %>%
+    slice(1) %>%
+    ungroup() %>%
+    select(record_id, ca_seq, dx_cpt_rep_yrs)
+  
+  return(rtn)
+}
