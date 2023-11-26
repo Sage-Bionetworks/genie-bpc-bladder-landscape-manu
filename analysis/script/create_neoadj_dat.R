f68936b620e6f7b0d9b029dea79fb50f6e0dc8f9
alexpaynter <alexcpaynter@gmail.com>
2023-11-25 20:16:32 -0800

    Neoadjuvant categories done

--- analysis/script/create_neoadj_dat.R
+++ analysis/script/create_neoadj_dat.R
@@ -15,7 +15,7 @@ dft_reg <- readr::read_rds(
 # This file was created by going through the list of regimens at a meeting 
 #   one day.  It's totally preference of the team working on this paper.
 dft_neoadj <- readr::read_csv(
-  here('data', 'drug', 'neoadjuvant_regimens_2023_11_06.csv')
+  here('data', 'drug_mapping', 'neoadjuvant_regimens_2023_11_06.csv')
 )
 
 # Ensure all entries are 'yes' or 'no' only.
@@ -54,7 +54,7 @@ dft_neoadj %<>%
 # write a copy of this:
 readr::write_rds(
   x = dft_neoadj,
-  file = here('data', 'drug', 'neoadjuvant_reg_key.rds')
+  file = here('data', 'drug_mapping', 'neoadjuvant_reg_key.rds')
 )
 
 
@@ -86,35 +86,72 @@ dft_reg_neo <- left_join(
   by = c("record_id", "ca_seq")
 )
 
-
-
 dft_reg_neo %<>%
   filter(!str_detect(regimen_drugs, "Investigational Drug")) %>%
   mutate(
-    tt_met_reg_start = dmet_time_yrs - dx_reg_start_int_yrs,
-    tt_met_reg_end = dmet_time_yrs - dx_reg_end_all_int_yrs,
+    tt_met_reg_start_yrs = dmet_time_yrs - dx_reg_start_int_yrs,
+    tt_met_reg_end_yrs = dmet_time_yrs - dx_reg_end_all_int_yrs,
     is_neoadjuvant = case_when(
       is.na(dmet_time_yrs) ~ NA,
       # this can still be NA for regimens that appeared only after met
       #. (these regimens were never presented to physicians for review)
       is.na(valid_neoadjuvant) ~ NA,
       # small tolerance here - half a day - for rounding errors.
-      valid_neoadjuvant %in% T & tt_met_reg_start >= -(0.5/365.25) ~ T,
+      valid_neoadjuvant %in% T & tt_met_reg_start_yrs >= -(0.5/365.25) ~ T,
       T ~ F
     )
   )
 
+lev_met_neoadj <- c(
+  "Neoadjuvant, ended <=6 months prior to met",
+  "Neoadjuvant, ended >6 months prior to met",
+  "Not neoadjuvant treatment"
+)
 
-# dft_reg_neo_cases <- dft_reg_neo %>%
-#   group_by(record_id, ca_seq) %>%
-#   mutate(
-#     is_gte_6mo = case_when(
+dft_reg_neo %<>%
+  mutate(
+    is_gt_6mo = tt_met_reg_end_yrs > 0.5,
+    is_lte_6mo = tt_met_reg_end_yrs <= 0.5,
+    met_neoadj_f = case_when(
+      is.na(is_neoadjuvant) ~ lev_met_neoadj[3],
+      !is_neoadjuvant ~ lev_met_neoadj[3],
+      is_neoadjuvant & is_gt_6mo ~ lev_met_neoadj[2],
+      is_neoadjuvant & is_lte_6mo ~ lev_met_neoadj[1],
+      # These are backup designations. If we don't have the end date, we go
+      #   with what the start date tells us.
+      is_neoadjuvant & (tt_met_reg_start_yrs <= 0.5) ~ lev_met_neoadj[1],
+      T ~ lev_met_neoadj[2], # definitely debatable.
+    ),
+    met_neoadj_f = factor(met_neoadj_f, levels = lev_met_neoadj)
+  )
+
+readr::write_rds(
+  dft_reg_neo,
+  file = here('data', 'cohort', 'neoadjuvant_status_case.rds')
+)
+
+# The case labels are similar, just need to update the last one:
+lev_met_neoadj_cases <- lev_met_neoadj
+lev_met_neoadj_cases[3] <- "No neoadjuvant treatment"
+
+dft_reg_neo_cases <- dft_reg_neo %<>%
+  group_by(record_id, ca_seq) %>%
+  summarize(
+    met_neoadj_case_f = case_when(
+      any(met_neoadj_f %in% lev_met_neoadj[1]) ~ lev_met_neoadj_cases[1],
+      any(met_neoadj_f %in% lev_met_neoadj[2]) ~ lev_met_neoadj_cases[2],
+      T ~ lev_met_neoadj_cases[3]
+    ),
+    .groups = "drop"
+  )
+
+readr::write_rds(
+  dft_reg_neo_cases,
+  file = here('data', 'cohort', 'neoadjuvant_status_case.rds')
+)
       
 
 # Todo:
-#  - tag regimens into being neoadjuvant, gte 6 months, lt 6 months.
-#  - create a cancer-level dataframe classifying each cancer into the
-#      groups laid out in the paper.
 #  - update the manuscript sections to have 3 tables.
 #  - remove repeats in the tables if not already done.
 #  - remove same-class regimens in the table (e.g. carboplatin and cisplatin)
--- todo.md
+++ todo.md
@@ -2,3 +2,6 @@
 - Update the neoadjuvant script to use a derived version of the regimen variable
     which uses "end_or_lastadm" version instead of "end_int".   This can be copied
     over from the off label manuscript paper.
+- update the manuscript sections to have 3 tables.
+- remove repeats in the tables if not already done.
+- remove same-class regimens in the table (e.g. carboplatin and cisplatin)
