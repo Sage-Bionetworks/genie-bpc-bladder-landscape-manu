c0717a69e52b9624a153be582f52aad73a909ec5
alexpaynter <alexcpaynter@gmail.com>
2024-02-25 17:50:06 -0800

    reconfigured neoadjuvant derivations

--- analysis/script/create_neoadj_dat.R
+++ analysis/script/create_neoadj_dat.R
@@ -13,21 +13,24 @@ dft_reg <- readr::read_rds(
 )
 
 # This file was created by going through the list of regimens at a meeting 
-#   one day.  It's totally preference of the team working on this paper.
+#   one day.  The list was expanded on Feb 21 using the method of statistician
+#   guesswork.
 dft_neoadj <- readr::read_csv(
   here('data', 'drug_mapping', 'neoadjuvant_regimens_2024_02_21.csv')
 )
 
 dft_neoadj %<>% select(regimen_drugs, valid_neoadjuvant) # leaves off comments
 
+dft_neoadj %<>% rename(valid_neoadjuvant_drug = valid_neoadjuvant) # for clarity
+
 # Ensure all entries are 'yes' or 'no' only.
 chk_neoadj_col <- dft_neoadj %>%
-  pull(valid_neoadjuvant) %>%
+  pull(valid_neoadjuvant_drug) %>%
   `%in%`(., c("yes", "no")) %>%
   all(.)
 
 if (!chk_neoadj_col) {
-  cli::cli_abort("Invalid entries in the input neoadjuvant CSV (valid_neoadjuvant column)")
+  cli::cli_abort("Invalid entries in the input neoadjuvant CSV (valid_neoadjuvant_drug column)")
 }
 
 
@@ -48,9 +51,9 @@ if (!chk_reg_drug_col) {
 dft_neoadj %<>%
   arrange(regimen_drugs) %>%
   mutate(
-    valid_neoadjuvant = case_when(
-      valid_neoadjuvant %in% "yes" ~ T,
-      valid_neoadjuvant %in% "no" ~ F,
+    valid_neoadjuvant_drug = case_when(
+      valid_neoadjuvant_drug %in% "yes" ~ T,
+      valid_neoadjuvant_drug %in% "no" ~ F,
       T ~ NA
     )
   )
@@ -61,10 +64,6 @@ readr::write_rds(
   file = here('data', 'drug_mapping', 'neoadjuvant_reg_key.rds')
 )
 
-
-
-# Todo: Update to the new definition of neoadjuvant.  Make sure it applies to
-#   all participants including those without mets.
 
 
 
@@ -88,7 +87,7 @@ dft_reg_neo %<>%
     ca_seq,
     regimen_number,
     regimen_drugs,
-    valid_neoadjuvant,
+    valid_neoadjuvant_drug,
     dx_reg_start_int_yrs,
     dx_reg_end_all_int_yrs
   )
@@ -103,23 +102,36 @@ dft_reg_neo <- left_join(
   by = c("record_id", "ca_seq")
 )
 
-# Update:  This table makes little to no sense for people who never reached
-#  metastatic disease.  We filter to avoid confusion.
 dft_reg_neo %<>%
-  filter(!is.na(dmet_time_yrs))
+  mutate(
+    met_ever = if_else(is.na(dmet_time_yrs), F, T)
+  )
+
+
+
+
 
 dft_reg_neo %<>%
   filter(!str_detect(regimen_drugs, "Investigational Drug")) %>%
   mutate(
     tt_met_reg_start_yrs = dmet_time_yrs - dx_reg_start_int_yrs,
-    tt_met_reg_end_yrs = dmet_time_yrs - dx_reg_end_all_int_yrs,
+    tt_met_reg_end_yrs = dmet_time_yrs - dx_reg_end_all_int_yrs
+  )
+
+# We've updated our defintion.  Neoadjuvant is now defined by a drug use 
+#   which is on our list of regimens (platinum-based chemo) AND either:
+#   1. drug use started more than 3 months before met diagnosis.
+#   2. the participant never developed metastasis.
+# There are some obvious problems with this, notably that the classification
+#   of a drug as met or not could change if the person is diagnosed soon 
+#.  (in that three month no man's land).
+
+dft_reg_neo %<>%
+  mutate(
     is_neoadjuvant = case_when(
-      is.na(dmet_time_yrs) ~ NA,
-      # this can still be NA for regimens that appeared only after met
-      #  (these regimens were never presented to research team for review)
-      is.na(valid_neoadjuvant) ~ NA,
-      # small tolerance here - half a day - for rounding errors.
-      valid_neoadjuvant %in% T & tt_met_reg_start_yrs >= -(0.5/365.25) ~ T,
+      !met_ever & valid_neoadjuvant_drug ~ T, # new case
+      # small tolerance here, half a day, for rounding errors.
+      valid_neoadjuvant_drug %in% T & tt_met_reg_start_yrs >= 0.25 - (0.5/365.25) ~ T,
       T ~ F
     )
   )
@@ -139,31 +151,26 @@ if (nrow(dft_multi_neo)) {
 
 
 
-lev_met_neoadj <- c(
-  "Neoadjuvant, ended <=6 months prior to met",
-  "Neoadjuvant, ended >6 months prior to met",
+lev_neoadj <- c(
+  "Neoadjuvant, started >=3 months prior to met",
+  "Neoadjuvant, never-met patient",
   "Not neoadjuvant treatment"
 )
 
 dft_reg_neo %<>%
   mutate(
-    is_gt_6mo = tt_met_reg_end_yrs > 0.5,
-    is_lte_6mo = tt_met_reg_end_yrs <= 0.5,
-    
-    met_neoadj_f = case_when(
-      is.na(is_neoadjuvant) ~ lev_met_neoadj[3],
-      !is_neoadjuvant ~ lev_met_neoadj[3],
-      # judgement call here
-      # if the end date is unspecified we assume it could have been within 6 months of met.
-      is.na(is_gt_6mo) ~ lev_met_neoadj[1],
-      is_neoadjuvant & is_gt_6mo ~ lev_met_neoadj[2],
-      is_neoadjuvant & is_lte_6mo ~ lev_met_neoadj[1],
+    neoadj_f = case_when(
+      is.na(is_neoadjuvant) ~ lev_neoadj[3],
+      !is_neoadjuvant ~ lev_neoadj[3],
+      
+      is_neoadjuvant & met_ever ~ lev_neoadj[1],
+      is_neoadjuvant & !met_ever ~ lev_neoadj[2],
       T ~ NA_character_ # hopefully none.
     ),
-    met_neoadj_f = factor(met_neoadj_f, levels = lev_met_neoadj)
+    neoadj_f = factor(neoadj_f, levels = lev_neoadj)
   )
 chk_met_neoadj_f <- dft_reg_neo %>% 
-  filter(is.na(met_neoadj_f)) %>%
+  filter(is.na(neoadj_f)) %>%
   nrow(.)
 
 if (chk_met_neoadj_f > 0) {
@@ -178,38 +185,50 @@ readr::write_rds(
 )
 
 
-lev_met_neoadj_cases <- c(
-  "Neoadjuvant, some ended <=6 months prior to met",
-  "Neoadjuvant, all ended >6 months prior to met",
-  "No neoadjuvant treatment"
-)
+lev_neoadj_case <- lev_neoadj
+lev_neoadj_case[3] <- "No neoadjuvant treatment"
 
-dft_reg_neo_cases <- dft_reg_neo %<>%
+dft_reg_neo_cases <- dft_reg_neo %>%
   group_by(record_id, ca_seq) %>%
   summarize(
-    met_neoadj_case_f = case_when(
-      any(met_neoadj_f %in% lev_met_neoadj[1]) ~ lev_met_neoadj_cases[1],
-      any(met_neoadj_f %in% lev_met_neoadj[2]) ~ lev_met_neoadj_cases[2],
-      T ~ lev_met_neoadj_cases[3]
+    neoadj_case_f = case_when(
+      any(neoadj_f %in% lev_neoadj[1]) ~ lev_neoadj_case[1],
+      any(neoadj_f %in% lev_neoadj[2]) ~ lev_neoadj_case[2],
+      T ~ lev_neoadj_case[3]
+    ),
+    tt_met_first_neoadj_start = suppressWarnings(
+      min(tt_met_reg_start_yrs[is_neoadjuvant], na.rm = T)
     ),
     .groups = "drop"
   )
 
-# One more thing needs to be done here to make this more sensible:  We need to 
-#   add back in the people who had zero regimens before met.  Then they will be 
-#   correctly added as having no neoadjuvant therapy. 
+# Fix the +Inf returns from min above:
+dft_reg_neo_cases %<>% 
+  mutate(
+    tt_met_first_neoadj_start = if_else(
+      is.infinite(tt_met_first_neoadj_start),
+      NA_real_,
+      tt_met_first_neoadj_start
+    )
+  )
+      
+  
 
-dft_no_reg_addins <- dft_dmet_timing %>%
+# One more thing needs to be done here to make this more sensible:  We need to 
+#   add back in the people who had zero regimens.  Then they will be 
+#   correctly coded as having no neoadjuvant therapy. 
+
+dft_no_reg_addins <- dft_ca_ind %>%
   filter(!(record_id %in% dft_reg_neo_cases$record_id)) %>%
   select(record_id, ca_seq) %>%
   mutate(
-    met_neoadj_case_f = lev_met_neoadj_cases[3]
+    neoadj_case_f = lev_neoadj_case[3]
   )
 
 dft_reg_neo_cases %<>%
   bind_rows(., dft_no_reg_addins) %>%
   mutate(
-    met_neoadj_case_f = factor(met_neoadj_case_f, levels = lev_met_neoadj_cases)
+    neoadj_case_f = factor(neoadj_case_f, levels = lev_neoadj_case)
   )
 
 readr::write_rds(
