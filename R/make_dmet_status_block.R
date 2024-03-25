92bde4e747a1b3521e598bad1b6d0d7850f6ea9a
alexpaynter <alexcpaynter@gmail.com>
2024-03-25 08:53:38 -0700

    Fixed function (never used) related to mets

--- R/make_dmet_status_block.R
+++ R/make_dmet_status_block.R
@@ -11,39 +11,42 @@ make_dmet_status_block <- function(ca_ind_dat) {
       ca_seq,
       stage_dx_iv,
       ca_dmets_yn,
-      dx_to_dmets_yrs,
+      # dx_to_dmets_yrs, # not ever used, need helper.
       tt_os_dx_yrs
     )
   
-  dmet_at_onset <- ci_sub %>%
-    filter(stage_dx_iv %in% "Stage IV" & ca_dmets_yn %in% "Yes") %>%
+  dmet_types <- get_dmet_time(ca_ind_dat, annotate_type = T)
+  
+  dmet_at_onset <- dmet_types %>%
+    filter(.met_type %in% "stage_iv_with_immediate_dmet") %>%
+    select(record_id, ca_seq) %>%
+    left_join(., ci_sub, by = c("record_id", "ca_seq")) %>%
     mutate(dmet_status = dmet_stat_levs[2],
            dx_block_start = 0,
            dx_block_end = tt_os_dx_yrs) 
   
-  dmet_in_fu <- ci_sub %>%
-    # A note on this filtering: There is at least one person who was diagnosed
-    #   as stage 4 but has dmets_stage_i_iii == 1.  Instead of messing with
-    #   those variables, we're just taking the complement of the previous filter
-    #   and then filtering down to anyone with a dmet time listed - that way
-    #   we get everyone with usable information.
-    filter(!(stage_dx_iv %in% "Stage IV" & ca_dmets_yn %in% "Yes")) %>%
-    filter(!is.na(dx_to_dmets_yrs)) %>%
-    # for each person in this subset we need 2 blocks: dx to dmet and dmet to death/censor.
+  dmet_in_fu <- dmet_types %>%
+    filter(!(.met_type %in% "stage_iv_with_immediate_dmet")) %>%
+    # dx_dmet_yrs my derived version of the flawed dx_to_dmets_yrs.  See get_dmet_time() for construction.
+    select(record_id, ca_seq, dx_dmet_yrs) %>%
+    left_join(., ci_sub, by = c("record_id", "ca_seq")) %>%
     group_by(record_id, ca_seq) %>%
+    # need two blocks for each of these people:
     slice(rep(1:n(), each = 2)) %>%
     mutate(
       dmet_status = if_else(row_number() %in% 1,
                             dmet_stat_levs[1],
                             dmet_stat_levs[2]),
-      dx_block_start = if_else(row_number() %in% 1, 0, dx_to_dmets_yrs),
-      dx_block_end = if_else(row_number() %in% 1, dx_to_dmets_yrs, tt_os_dx_yrs)
-    ) 
+      dx_block_start = if_else(row_number() %in% 1, 0, dx_dmet_yrs),
+      dx_block_end = if_else(row_number() %in% 1, dx_dmet_yrs, tt_os_dx_yrs)
+    ) %>%
+    ungroup(.)
   
-  dmet_never <- ci_sub %>%
-    # See note on filtering above.
-    filter(!(stage_dx_iv %in% "Stage IV" & ca_dmets_yn %in% "Yes")) %>%
-    filter(is.na(dx_to_dmets_yrs)) %>%
+  dmet_never <- anti_join(
+    ci_sub,
+    dmet_types,
+    by = c("record_id", "ca_seq")
+  ) %>%
     mutate(dmet_status = dmet_stat_levs[1],
            dx_block_start = 0,
            dx_block_end = tt_os_dx_yrs) 
