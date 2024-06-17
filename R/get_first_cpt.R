0c2c1c1a5ff79dc5058b6e13290f93e8348127b6
alexpaynter <alexcpaynter@gmail.com>
2024-06-17 13:28:16 -0700

    Several adds on survival analyses

--- R/get_first_cpt.R
+++ R/get_first_cpt.R
@@ -1,12 +1,17 @@
-get_first_cpt <- function(ca_ind_dat, cpt_dat) {
+get_first_cpt <- function(ca_ind_dat, cpt_dat, include_sample_id = F) {
   rtn <- ca_ind_dat %>% 
     select(record_id, ca_seq) %>%
     left_join(., cpt_dat, by = c("record_id", "ca_seq")) %>%
     arrange(cpt_number) %>%
     group_by(record_id) %>%
     slice(1) %>%
-    ungroup() %>%
-    select(record_id, ca_seq, dx_cpt_rep_yrs)
+    ungroup() 
+  
+  if (include_sample_id) {
+    rtn %<>% select(record_id, ca_seq, dx_cpt_rep_yrs, cpt_genie_sample_id)
+  } else {
+    rtn %<>% select(record_id, ca_seq, dx_cpt_rep_yrs, cpt_genie_sample_id)
+  }
   
   return(rtn)
 }
