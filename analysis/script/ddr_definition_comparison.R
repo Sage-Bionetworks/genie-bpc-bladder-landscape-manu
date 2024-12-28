f244e20d25752c43fdf35d758b55450e3007286c
alexpaynter <alexcpaynter@gmail.com>
2024-12-28 11:16:18 -0800

    About 2/3 done with the initial investigation set - setting up report

--- analysis/script/ddr_definition_comparison.R
+++ analysis/script/ddr_definition_comparison.R
@@ -1,12 +1,19 @@
 library(purrr); library(here); library(fs)
 purrr::walk(.x = fs::dir_ls(here('R')), .f = source)
 
+dir_out <- here('data', 'genomic', 'ddr_def_compare')
 
 sample_main <- readr::read_tsv(
   here('data-raw', 'genomic', 'main_genie', 'data_clinical_sample.txt'),
   skip = 4
 ) %>%
   rename_all(tolower)
+
+bladder_samp_mg <- readr::read_rds(
+  here('data', 'genomic', 'main_genie', 'bladder_samples_mg.rds')
+)
+
+sample_main %<>% filter(sample_id %in% bladder_samp_mg)
 
 alt_main <- readr::read_rds(
   here('data', 'genomic', 'main_genie', 'alterations.rds')
@@ -39,14 +46,58 @@ ddr_def_pearl_addons <- bind_rows(
 
 ddr_def <- bind_rows(ddr_def, ddr_def_pearl_addons)
 
+# One more:  The genes covered in 100% of panels in GENIE BPC, and included in
+#    at least one of the four UC papers discussing this.
+
+gp_all <- readr::read_rds(
+  here('data', 'genomic', 'gene_panel_all.rds')
+)
+
+uc_ddr_author_genes <- ddr_def %>%
+  filter(!str_detect(source, 'pearl')) %>%
+  pull(gene) %>%
+  unique(.)
+
+# There are some of these genes that appear in 0% of our panels:
+# gp_all %>%
+#   pull(hugo) %>%
+#   unique(.) %>%
+#   as.character(.) %>%
+#   setdiff(uc_ddr_author_genes, .)
+
+gp_ddr_coverage <- gp_all %>%
+  group_by(cpt_seq_assay_id) %>%
+  summarize(
+    gene_overlap = list(intersect(as.character(hugo), # genes in this panel
+                                  uc_ddr_author_genes)), # genes in the union of all papers about this.
+    .groups = 'drop'
+  ) %>% 
+  mutate(overlap_size = purrr::map_dbl(.x = gene_overlap, .f = \(x) length(unique(x))))
+
+genie_intersect_panel <- gp_ddr_coverage %>%
+  # ad hoc sloppy decision to exclude some of the outlier-tiny panels:
+  filter(overlap_size >= 5) %>%
+  pull(gene_overlap) %>%
+  purrr::reduce(.x = ., .f = intersect)
+
+ddr_def <- bind_rows(
+  ddr_def,
+  tibble(
+    source = 'GENIE int 2024',
+    gene = genie_intersect_panel,
+  )
+)
+
+
+
+
+
 ddr_def_plot <- ddr_def %>%
   filter(!str_detect(source, 'pearl')) %>%
   mutate(year = readr::parse_number(source)) %>%
   arrange(year) %>%
   mutate(source_f = fct_inorder(source))
 
-cli_alert_warning("One more I want to add:  A very small subset covered by all GENIE panels")
-  
 # first just a basic comparison of what these defs cover:
 ddr_def_plot <- ddr_def_plot %>%
   select(-`author affiliation`) %>%
@@ -64,22 +115,22 @@ gg_def_compare <- ggplot(
   theme_classic() +
   theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
-gg_def_compare
+# gg_def_compare
 
 readr::write_rds(
   gg_def_compare,
-  here('data', 'genomic', 'gg_def_compare_heatmap.rds')
+  here(dir_out, 'gg_def_compare_heatmap.rds')
 )
 
 
 
-ddr_def %>%
+ddr_def_nest <- ddr_def %>%
   select(source, gene) %>%
   nest(.by = source) %>%
   mutate(
-    gene_list = purrr::map(.x = data, .f = \(x) list(pull(x,gene)))
+    gene_list = purrr::map(.x = data, .f = \(x) pull(x,gene))
   ) %>%
-  select(-data) %>% View(.)
+  select(-data)
 
 first_sample_main <- sample_main %>% 
   group_by(patient_id) %>%
@@ -91,5 +142,176 @@ first_sample_main <- sample_main %>%
   ungroup(.)
 
 
-# Next steps:  Make a function to count the number of first samples (two datasets, sample list and alterations) which have at least one ddr mutation based on a list.  Then process that for all the various definitions and count it up.
+# Example of using once:    
+# count_pts_gene_list(
+#   sample_df = first_sample_main,
+#   alt_df = alt_main,
+#   gene_list = pull(slice(ddr_def_nest, 5), gene_list)[[1]]
+# ) %>%
+#   count(any_alt)
+
+ddr_def_nest %<>%
+  mutate(
+    alt_binary = purrr::map(
+      .x = gene_list,
+      .f = \(x) count_pts_gene_list(
+        sample_df = first_sample_main,
+        alt_df = alt_main,
+        gene_list = x
+      )
+    ),
+    alt_binary_onco = purrr::map(
+      .x = gene_list,
+      .f = \(x) count_pts_gene_list(
+        sample_df = first_sample_main,
+        alt_df = filter(alt_main, oncogenic %in% c("Likely Oncogenic", "Oncogenic")),
+        gene_list = x
+      )
+    )
+  )
+
+pt_counts_ddr_def <- ddr_def_nest %>%
+  select(source, alt_binary, alt_binary_onco) %>%
+  pivot_longer(
+    cols = c(alt_binary, alt_binary_onco),
+    names_to = 'onco_filter',
+    values_to = 'pt_df'
+  ) %>%
+  mutate(
+    str = purrr::map_chr(
+      .x = pt_df,
+      .f = \(x) {x %>%
+        summarize(
+          n = sum(any_alt),
+          d = n()
+        ) %>%
+        mutate(str = glue("{round((n/d)*100)}% ({n})")) %>%
+        pull(str)
+      }
+    ),
+    n_pts = purrr::map_dbl(.x = pt_df, .f = nrow)
+  )
+
+pt_counts_ddr_def %<>%
+  mutate(
+    data = 'GENIE-UC',
+    onco_filter = case_when(
+      str_detect(onco_filter, 'onco') ~ T,
+      T ~ F
+    )
+  ) %>%
+  select(data, onco_filter, source, str, n_pts)
+      
+
+
+
+
+
+
+# Load in the cohort file from BPC and repeat this process for BPC.
+cpt <- readr::read_rds(here('data', 'cohort', 'cpt.rds'))
+
+alt_bpc <- readr::read_rds(
+  here('data', 'genomic', 'alterations.rds')
+)
+
+# just checking:
+# setdiff(cpt$cpt_genie_sample_id, sample_main$sample_id)
+ddr_def_nest_bpc <- ddr_def_nest %>% select(source, gene_list)
+
+first_sample_bpc <- first_sample_main %>%
+  filter(sample_id %in% cpt$cpt_genie_sample_id)
+
+ddr_def_nest_bpc %<>%
+  mutate(
+    alt_binary = purrr::map(
+      .x = gene_list,
+      .f = \(x) count_pts_gene_list(
+        sample_df = first_sample_bpc,
+        alt_df = alt_bpc,
+        gene_list = x
+      )
+    ),
+    alt_binary_onco = purrr::map(
+      .x = gene_list,
+      .f = \(x) count_pts_gene_list(
+        sample_df = first_sample_bpc,
+        alt_df = filter(alt_bpc, oncogenic %in% c("Likely Oncogenic", "Oncogenic")),
+        gene_list = x
+      )
+    )
+  )
+
+pt_counts_ddr_def_bpc <- ddr_def_nest_bpc %>%
+  select(source, alt_binary, alt_binary_onco) %>%
+  pivot_longer(
+    cols = c(alt_binary, alt_binary_onco),
+    names_to = 'onco_filter',
+    values_to = 'pt_df'
+  ) %>%
+  mutate(
+    str = purrr::map_chr(
+      .x = pt_df,
+      .f = \(x) {x %>%
+          summarize(
+            n = sum(any_alt),
+            d = n()
+          ) %>%
+          mutate(str = glue("{round((n/d)*100)}% ({n})")) %>%
+          pull(str)
+      }
+    ),
+    n_pts = purrr::map_dbl(.x = pt_df, .f = nrow)
+  )
+
+pt_counts_ddr_def_bpc %<>%
+  mutate(
+    data = 'GENIE-BPC',
+    onco_filter = case_when(
+      str_detect(onco_filter, 'onco') ~ T,
+      T ~ F
+    )
+  ) %>%
+  select(data, onco_filter, source, str, n_pts)
+
+pt_counts_ddr_def <- bind_rows(
+  pt_counts_ddr_def,
+  pt_counts_ddr_def_bpc
+)
+
+readr::write_rds(
+  pt_counts_ddr_def, 
+  here(dir_out, 'pt_count_tidy.rds')
+)
+
+ft_ddr_def <- pt_counts_ddr_def %>%
+  mutate(
+    col_title = paste0(data, '|',
+                      '(n = ', n_pts, ')', '|',
+                      if_else(onco_filter, "Oncogenic", 'Any alt.'))
+  ) %>%
+  select(source, col_title, str) %>%
+  pivot_wider(
+    names_from = 'col_title',
+    values_from = 'str'
+  ) 
+
+new_header <- names(ft_ddr_def) %>% 
+  tibble(col_keys = .) %>%
+  separate_wider_delim(names = c('one', 'two', 'three'),
+                       col_keys, delim = '|', cols_remove = F,
+                       too_few = 'align_start')
+
+ft_ddr_def %>%
+  flextable(.) %>%
+  flextable::set_header_df(mapping = new_header, key = 'col_keys') %>%
+  theme_booktabs(.) %>%
+  autofit(.)
+
+readr::write_rds(
+  ft_ddr_def,
+  here(dir_out, 'ft_ddr_def.rds')
+)
+  
+            
 
