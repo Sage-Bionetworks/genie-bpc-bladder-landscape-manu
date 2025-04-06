d49447ea558c0893469509e8067f36bb4210a62a
alexpaynter <alexcpaynter@gmail.com>
2025-04-06 11:35:59 -0700

    deleted erroneous python script for oncoPrints - it was R all along just took me a minute to recognize

--- analysis/explore/oncoPrint_test.py
+++
@@ -1,7 +0,0 @@
-mat = read.table(textConnection(
-"s1,s2,s3
-g1,snv;indel,snv,indel
-g2,,snv;indel,snv
-g3,snv,,indel;snv"), row.names = 1, header = TRUE, sep = ",", stringsAsFactors = FALSE)
-mat = as.matrix(mat)
-mat
