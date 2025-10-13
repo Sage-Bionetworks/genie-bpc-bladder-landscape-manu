66d966bc6c8feb5a6bbe7ee1e2d0eb227f31f794
alexpaynter <alexcpaynter@gmail.com>
2025-10-13 00:10:03 -0700

    Trimming folder setup a bit

--- analysis/script/folder_setup.R
+++ analysis/script/folder_setup.R
@@ -19,13 +19,7 @@ dir_create('output', 'other')
 dir_create('output', 'aacr_ss24', 'tfl_obj')
 dir_create('output', 'aacr_ss24', 'img')
 
-fs::dir_create(here('data', 'survival'))
-fs::dir_create(here('data', 'survival', 'first_line_platinum'))
-fs::dir_create(here('data', 'survival', 'hrd_onco'))
-fs::dir_create(here('data', 'survival', 'ercc3_plat'))
-fs::dir_create(here('data', 'survival', 'first_line_immuno'))
-fs::dir_create(here('data', 'survival', 'ddr_onco'))
-fs::dir_create(here('data', 'survival', 'ddr_neoadj'))
-fs::dir_create(here('data', 'survival', 'line23'))
+fs::dir_create(here('data', 'survival', 'ddr_onco_1L'))
+
 
 fs::dir_create(here('data', 'genomic', 'ddr_def_compare', 'ddr_as_outcome'))
